1019fa9aa53c658a205bce863872a8c33e09b700
aizere77 <aizerekalmakanova7@gmail.com>
2026-01-30 16:07:24 +0500

    syntax

---
+++ python-basics/syntax.py
@@ -0,0 +1,23 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+"""
+if 5 > 2:
+print("Five is greater than two!")         #Syntax Error
+"""
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+"""
+print("Python is fun!") print("Really!")       #invalid syntax
+"""
+if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+"""
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+"""
