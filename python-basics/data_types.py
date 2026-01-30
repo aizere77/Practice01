3aee28cb90bbf88a280d97b35368a52853009276
aizere77 <aizerekalmakanova7@gmail.com>
2026-01-30 11:08:23 +0500

    variables

---
+++ python-basics/data_types.py
@@ -0,0 +1,12 @@
+#Setting the data type
+x = "Hello World" #str
+x = 12 #int
+x = 12.3 #float
+x = 12j #complex
+x = ["a", "b", "c"] #list
+x = ("a", "b", "c") #turple
+x = range(6) #range
+x = {"name" : "Aizere", "age" : 17} #dict
+x = {"a", "b", "c"} #set
+x = True #bool
+x = b"Hello" #bytes
---
+++ python-basics/variables.py
@@ -0,0 +1,32 @@
+#A variable is created the moment you first assign a value to it.
+x = 5
+y = "Aizere"
+print(x)
+print(y)
+
+x = 5
+y = "Aizere"
+print(type(x))
+print(type(y))
+
+x = 4       # x is of type int
+x = "dbhduyf" # x is now of type str
+print(x)
+
+
+x = "Aizere"
+# is the same as
+x = 'Aizere'
+
+#This will create two variables:
+a = 4
+A = "vfky"
+#A will not overwrite a
+
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+
