3e1eb12a10d47778a7ca5a807efd26b73d64b688
aizere77 <aizerekalmakanova7@gmail.com>
2026-01-30 10:45:58 +0500

    numbers

---
+++ python-basics/numbers.py
@@ -0,0 +1,65 @@
+#NUMBERS 
+x = 1   # int
+y = 1.3  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#INT
+x = 13348960
+y = -7463738
+z = 1
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#FLOAT
+x = 1.10
+y = 1.0
+z = -3.5
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#COMPLEX
+x = 4+5j
+y = 2j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#CONVERSION
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
