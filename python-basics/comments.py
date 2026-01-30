bb44aa5751e32c8502ebc5dd9feeb4f96424a94c
aizere77 <aizerekalmakanova7@gmail.com>
2026-01-30 15:50:34 +0500

    comments

--- python-basics/comments.py
+++ python-basics/comments.py
@@ -1,2 +1,11 @@
 #This is a comment
 print("Hello, World!")
+print("Hello, World!") #This is a comment
+#print("Hello, World!")
+print("Cheers!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
---
+++ python-basics/strings.py
@@ -0,0 +1,23 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#You can use three double or single quotes
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#In the result, the line breaks are inserted at the same position as in the code.
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
